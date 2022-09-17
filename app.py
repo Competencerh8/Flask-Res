5ebf573c9b1836968c54339d75e2355ca3f93a4f
competencerh8 <philippedion92@gmail.com>
2022-09-17 13:02:04 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, url_for, redirect
+from email.mime.text import MIMEText
+import smtplib
+from email.message import EmailMessage
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   return render_template("Index2.html")
+
+
+ 
+if __name__ == "__main__":
+    app.run_server(debug=True)
